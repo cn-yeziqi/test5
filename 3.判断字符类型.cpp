0c9bec4c2d206cbeecd29ea419f97b0ac57f1f66
cn夜子柒 <748665675@qq.com>
2021-05-20 18:29:58 +0800

    作业

---
+++ 3.判断字符类型.cpp
@@ -0,0 +1,20 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	for (char ch=0; ch != '#';)
+//	{
+//		printf("ÇëÊäÈë");
+//		ch = getchar();
+//		getchar();
+//		if (ch >= 'A' && ch <= 'Z')
+//			printf("´óÐ´×ÖÄ¸\n");
+//		else if (ch >= 'a' && ch <= 'z')
+//			printf("Ð¡Ð´×ÖÄ¸\n");
+//		else if (ch >= '0' && ch <= '9')
+//			printf("Êý×Ö\n");
+//		else
+//			printf("ÆäËû×Ö·û\n");
+//	}
+//	return 0;
+//}
---
+++ 5.100以内奇数相减之和.cpp
@@ -0,0 +1,17 @@
+//
+//#include <stdio.h>
+//int main()
+//{
+//	long x = 1;
+//	long sum = 0;
+//	long flag = 1;
+//	long i = 0;
+//	for (x = 1; x <= 101; x=x+2)
+//	{
+//		i = x * flag;
+//		sum += i;
+//		flag *= -1;
+//	}
+//	printf("sum=%ld", sum);
+//	return 0;
+//}
